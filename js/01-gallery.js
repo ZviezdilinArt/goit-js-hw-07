45998b9ec3e36494054dfd127cd3f3ab0a4f560e
ZviezdilinArt <118184844+ZviezdilinArt@users.noreply.github.com>
2023-04-02 16:34:43 +0300

    test commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const markupGallery = galleryItems.map(({preview, original, description}) =>  
+    `
+    <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `
+).join('');
+
+gallery.insertAdjacentHTML('beforeend', markupGallery);
+
+gallery.addEventListener('click', onClickImage);
+
+function onClickImage(evt) {
+    evt.preventDefault();
+    
+
+}
