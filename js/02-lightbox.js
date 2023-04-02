2923c12bc7b905fae6e6363a52a4edb0a16d7e57
ZviezdilinArt <118184844+ZviezdilinArt@users.noreply.github.com>
2023-04-02 20:34:13 +0300

    Task#2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector(".gallery");
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `
+  )
+  .join("");
+
+  galleryList.insertAdjacentHTML("beforeend", markupGallery);
+
+const options = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+const gallery = new SimpleLightbox(".gallery a", options);
+
+
+console.log(galleryList);
